86ceaf22ef4a3017eb34b0ce80f4d3abf3193694
sagi <sagidahan94@gmail.com>
2021-08-16 13:35:16 +0300

    cards and card component work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./components/PlayersCards";
+import PlayersCards from "./components/PlayersCards";
+import { playerForm } from "./api";
 
-function App() {
+const { playersData } = require("./playersdata.json");
+
+const App: React.FC = () => {
+  const [players, setPlayers] = useState<playerForm[]>(playersData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlayersCards players={players} />
     </div>
   );
-}
+};
 
 export default App;
